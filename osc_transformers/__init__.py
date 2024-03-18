8197706620e2e9728a265f7423ef8c33d940db38
wangmengdi <790990241@qq.com>
2024-03-18 09:52:56 -0400

    删除构建模型方法

--- osc_transformers/__init__.py
+++ osc_transformers/__init__.py
@@ -2,12 +2,3 @@ from .layers import *
 from .architectures import *
 from .config import registry, Config
 from .tokenizer import Tokenizer
-
-
-
-
-def build_model(name: str):
-    """Build a model from a name."""
-    config = registry.configs.get(name)()
-    model = registry.resolve(config)["model"]
-    return model
