09881bd5dd3f8e7970d295799057b469eea82f05
wangmengdi <790990241@qq.com>
2024-03-23 10:25:54 -0400

    导入新layer

--- osc_transformers/layers/__init__.py
+++ osc_transformers/layers/__init__.py
@@ -1,6 +1,7 @@
-from .norm import RMSNorm
-from .attention import CausalSelfAttention
+from .norm import RMSNorm, LayerNorm
+from .attention import SelfAttention
 from .head import LMHead
 from .embedding import TokenEmbedding
 from .feedforward import GLU, SwiGLU, MoE, GeGLU
 from .activation import ReLU, SiLU, GELU
+from .kv_cache import StaticKVCache
