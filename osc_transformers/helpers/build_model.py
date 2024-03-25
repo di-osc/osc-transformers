84cfb6b2dbe184bd3625c47475e35bf995ec6bc8
wangmengdi <790990241@qq.com>
2024-03-25 18:30:46 -0400

    添加了加载模型权重的方法 load_from_checkpoint

--- osc_transformers/helpers/build_model.py
+++ osc_transformers/helpers/build_model.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Dict, Union
 from ..config import Config, registry
+import torch
 
 
 
@@ -19,3 +20,23 @@ def build_from_config(config: Union[Dict, str, Path, Config], model_section: str
     if isinstance(config, dict):
         config = Config(data=config)
     return registry.resolve(config=config)[model_section]
+
+
+def load_from_checkpoint(checkpoint_dir: Union[str, Path], model_section: str = 'model', config_name: str = 'config.cfg', model_name: str = 'osc_model.pth'):
+    """Load a model from a checkpoint directory.
+
+    Args:
+        checkpoint_dir (Union[str, Path]): the directory containing the model checkpoint.
+        model_section (str, optional): the section to look for the model in the configuration. Defaults to 'model'.
+        config_name (str, optional): the name of the configuration file. Defaults to 'config.cfg'.
+        model_name (str, optional): the name of the model file. Defaults to 'osc_model.pth'.
+
+    Returns:
+        torch.nn.Module: the model loaded from the checkpoint.
+    """
+    checkpoint_dir = Path(checkpoint_dir)
+    config_path = Path(checkpoint_dir) / config_name
+    model = build_from_config(config_path, model_section=model_section)
+    states = torch.load(str(checkpoint_dir / model_name), map_location='cpu', mmap=True, weights_only=True)
+    model.load_state_dict(states)
+    return model
