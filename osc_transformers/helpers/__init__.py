96ab21c222b1e5871823fd5e01e3fad50cb1c213
wangmengdi <790990241@qq.com>
2024-04-14 23:31:52 -0400

    重新命名load_from_checkpoint -> build_from_checkpoint

--- osc_transformers/helpers/__init__.py
+++ osc_transformers/helpers/__init__.py
@@ -1,11 +1,11 @@
-from .build import build_from_config, load_from_checkpoint
+from .build import build_from_config, build_from_checkpoint
 from .huggingface import LlamaHelper, Qwen2Helper
 from .test import benchmark
 
 
 __all__ = [
     "build_from_config",
-    "load_from_checkpoint",
+    "build_from_checkpoint",
     "LlamaHelper",
     "Qwen2Helper",
     "benchmark",
