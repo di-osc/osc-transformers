4adfce4dcedbbd0f8cc89083828760201692fb81
wangmengdi <790990241@qq.com>
2025-09-19 04:46:17 +0800

    Refactor assertion in PagedAttention to improve readability

--- osc_transformers/attention/paged_attention.py
+++ osc_transformers/attention/paged_attention.py
@@ -79,9 +79,9 @@ class PagedAttention(CausalSelfAttention):
     ):
         super().__init__()
 
-        assert (
-            in_dim % num_heads == 0
-        ), f"dim {in_dim} must be divisible by num_heads {num_heads}"
+        assert in_dim % num_heads == 0, (
+            f"dim {in_dim} must be divisible by num_heads {num_heads}"
+        )
 
         self.num_heads = num_heads
         self.head_dim = head_dim or in_dim // num_heads
