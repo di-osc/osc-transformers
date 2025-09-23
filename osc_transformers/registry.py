3154459b75c808d949242df7823fedc7cde48d26
善若水 <790990241@qq.com>
2025-09-23 13:07:06 +0000

    Add quantizers to the Registry class for enhanced model support
    - Introduced a new entry point for quantizers in the Registry class, allowing for better integration of quantization functionalities within the OSC transformers framework.

--- osc_transformers/registry.py
+++ osc_transformers/registry.py
@@ -16,6 +16,7 @@ class Registry(confection.registry):
     head = catalogue.create("osc", "transformers", "head", entry_points=True)
     embedding = catalogue.create("osc", "transformers", "embedding", entry_points=True)
     sampler = catalogue.create("osc", "transformers", "sampler", entry_points=True)
+    quantizers = catalogue.create("osc", "transformers", "quantizers", entry_points=True)
 
 
 __all__ = ["Registry"]
